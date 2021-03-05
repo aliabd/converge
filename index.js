5abdf8ebd2abff564cc6a97e86697a9790ff6bbb
aliabd <ali.si3luwa@gmail.com>
2021-03-05 05:30:50 -0500

    some basic js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+var ai_tb = document.getElementById('ai-textbox');
+var us_tb = document.getElementById('user-textbox');
 
+function display_ai_guess(word) {
+    ai_tb.value = word;
+}
+
+function hide_words() {
+    ai_tb.value = "";
+    us_tb.value = "";
+}
+
+function bold() {
+    ai_tb.style.fontWeight = "900";
+    us_tb.style.fontWeight = "900";
+}
+
+function unbold() {
+    ai_tb.style.fontWeight = null;
+    us_tb.style.fontWeight = null;
+}
+
+function get_word(input) {
+    if(event.key === 'Enter') {
+        var word = input.value;
+        display_ai_guess("random");
+        bold();
+        setTimeout(() => { hide_words(); unbold();}, 2000);
+    }
+}
